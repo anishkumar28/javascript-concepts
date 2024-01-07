eb50ec8c6e6607b6daf0fbb454dd771ecd15b206
Anish Kumar <anishkumar6527@gmail.com>
2024-01-07 23:00:42 +0530

    Adding datatype file

---
+++ DataType.js
@@ -0,0 +1,20 @@
+// There are two categories of data type based on Storage & Acessiblity - Primitive data type and Refrerence(Non-primitive) data tye
+
+/* number, string, boolean, null, undefined, symbol & bigInt are primitive data types.
+
+ array, fucntions, Object literals & Dates are reference data types.
+
+*/
+
+
+var age  = 12; // number
+
+let name = "Om"; // string
+
+let isValue = false; // boolean
+
+var decision = null; // null
+
+let varName = undefined; // undefined
+
+
