96de09a59b83b028449ce3f58bca08c02e2783ec
Anish Kumar <anishkumar6527@gmail.com>
2024-01-10 23:53:57 +0530

    Init dataTpye conversion file

---
+++ DataType_Conversion.js
@@ -0,0 +1,11 @@
+// Number
+let score1 = 33
+console.log(typeof score1);
+
+// String
+let score2 = "33"
+console.log(typeof score2);
+
+// Changing data type from String to Number 
+let valueInNuber = Number(score2);
+console.log(typeof(valueInNuber));
