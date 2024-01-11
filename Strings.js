848155905de6be36968f459df179e4eeaea2effe
Anish Kumar <anishkumar6527@gmail.com>
2024-01-11 16:41:17 +0530

    including strings file

---
+++ Strings.js
@@ -0,0 +1,8 @@
+const name = "rehaan"
+const repoCount = 50
+
+// Traditional way of String (String concatination - Using plus sign to concatinate)
+console.log(name + repoCount + " Value");
+
+// Modern way of String (String Interpolation - Backticks are used, dollar sign are used for variable)
+console.log(`Hello my name is: ${name} and my repo count is ${repoCount} `);
