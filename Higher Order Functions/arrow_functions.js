d5787ba7fd43064fd2529f9611491d43fbc70158
Anish Kumar <anishkumar6527@gmail.com>
2024-01-14 21:56:04 +0530

    include arrow func file

---
+++ Higher Order Functions/arrow_functions.js
@@ -0,0 +1,13 @@
+//Creating Arrow function
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(addTwo(2, 4));
+
+
+// Implicit return arrow function (Implicit return meaning function argument is just one line, there is no need of return keyword in this. )
+const mulTwo = (num1, num2) => num1 * num2;// write directly 
+const subTwo = (num1, num2) => (num1 - num2);// wrap under paranthesis
+console.log(mulTwo(3, 7));
+console.log(subTwo(9, 5));
