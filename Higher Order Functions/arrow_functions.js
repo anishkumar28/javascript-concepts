28202d761d8405696b9d462d919367d239112b26
Anish Kumar <anishkumar6527@gmail.com>
2024-01-14 22:02:03 +0530

    objects in arrow func

--- Higher Order Functions/arrow_functions.js
+++ Higher Order Functions/arrow_functions.js
@@ -2,7 +2,6 @@
 const addTwo = (num1, num2) => {
     return num1 + num2;
 }
-
 console.log(addTwo(2, 4));
 
 
@@ -11,3 +10,8 @@ const mulTwo = (num1, num2) => num1 * num2;// write directly
 const subTwo = (num1, num2) => (num1 - num2);// wrap under paranthesis
 console.log(mulTwo(3, 7));
 console.log(subTwo(9, 5));
+
+
+//Objects in Arrow functions
+const divTwo = () => ({username: "hitesh"});
+console.log(divTwo());
