7f37ca662032b1855fe4486f866debff294057c0
Anish Kumar <anishkumar6527@gmail.com>
2024-01-16 20:15:13 +0530

    including fetch folder

---
+++ Fetch/fetch.js
@@ -0,0 +1,8 @@
+// Fetch is available through NodeJS or through Browser.
+// Fetch is a vip queue which go to call stack at first if no delay.
+
+fetch("https://dev.to/logrocket/the-fetch-api-is-finally-coming-to-nodejs-foe?comments_sort=oldest");
+
+
+
+// Documentation - "https://dev.to/logrocket/the-fetch-api-is-finally-coming-to-nodejs-foe?comments_sort=oldest"
